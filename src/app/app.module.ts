fabeb3a8a11c801af825db43d520e947bfba9431
Michael <michaelblxck@gmail.com>
2018-08-01 21:05:17 -0700

    Init.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
-
+import { jqxKnobComponent } from '../../node_modules/jqwidgets-scripts/jqwidgets-ts/angular_jqxknob';
+import { CompComponent } from './comp/comp.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    jqxKnobComponent,
+    CompComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
-})
+  bootstrap: [AppComponent]})
+
 export class AppModule { }
